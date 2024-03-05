adcf399c6aab31a8d9edecbd9eabf8e7c0608e55
THASNEEM VELLARAMPARA <60733459+THASNEEMMOOOSA@users.noreply.github.com>
2024-03-05 12:31:52 +0530

    Delete playground directory

--- playground/poll/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from poll.models import Person
-# Register your models here.
-
-class PersonAdmin(admin.ModelAdmin):
-    list_display=('id','name','age','city','dob')
-
-admin.site.register(Person,PersonAdmin)
